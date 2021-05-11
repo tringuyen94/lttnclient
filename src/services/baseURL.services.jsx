f81e5f6079d420c27a154716b17ba718f3cd89d2
tringuyen94 <58379108+tringuyen94@users.noreply.github.com>
2021-05-11 17:21:00 +0700

    Update port server

--- src/services/baseURL.services.jsx
+++ src/services/baseURL.services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const domain = "http://45.132.241.50:8080"
+export const domain = "http://45.132.241.50:9000"
 export const restConnector = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL || domain
 })
