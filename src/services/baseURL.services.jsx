dded145269192ff77f264d2ac2bad8f74ad3bbf7
tringuyen94 <58379108+tringuyen94@users.noreply.github.com>
2021-06-27 18:21:04 +0700

    change domain

--- src/services/baseURL.services.jsx
+++ src/services/baseURL.services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const domain = "http://45.132.241.50:9000"
+export const domain = "http://45.90.109.181:9000"
 export const restConnector = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL || domain
 })
